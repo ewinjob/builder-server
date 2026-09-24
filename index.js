9495995e7ca929fe5f14ce565e7f30524ac12cdc
ewin <ewinjob11@gmail.com>
2026-09-24 12:47:02 +0530

    server complete

---
+++ index.js
@@ -0,0 +1,18 @@
+const jsonserver = require('json-server');
+
+const server = jsonserver.create()
+
+const router = jsonserver.router('db.json')
+
+const middleware = jsonserver.defaults()
+
+const port = 3000
+
+ server.use(middleware)
+ server.use(router) 
+
+ server.listen(port,()=>{
+    console.log('JSON sever listening on port 3000');
+    
+ })
+ 
